1a77959c5d618f9c0460e84b1884102cb944a3f4
Hentsa999 <112977619+hentsa18@users.noreply.github.com>
2023-02-13 18:43:51 -0800

    the let and typeof

---
+++ 007-let/scripts/main.js
@@ -0,0 +1,4 @@
+let fname = "Hentsa";
+const myHeading = document.querySelector("h1");
+myHeading.textContent = fname;
+
---
+++ 008-variable-weak/scripts/main.js
@@ -0,0 +1,14 @@
+let foo = 42
+console.log("Foo is 42 - ", foo)
+
+foo = "bar"
+console.log("Foo is bar - ", foo)
+
+foo = true
+console.log("Foo is is true - ", foo)
+
+foo = 42
+const result = foo + '1'
+console.log("result is 42 + 1 ... ", result)
+
+
