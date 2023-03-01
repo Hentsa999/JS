2c3825584300d6eff391245faabc312dc5d84a3b
Hentsa999 <112977619+hentsa18@users.noreply.github.com>
2023-02-28 18:11:16 -0800

    learning about arrays

--- 011-more-functions/main.js
+++ 011-more-functions/main.js
@@ -1,7 +1,5 @@
 //Update the DOM
 
-const myArr = ["Hentsa", 19];
-
 const updateDOM = (input) => {
   const divEl = document.querySelector("#output");
   const p = document.createElement("p");
@@ -12,10 +10,13 @@ const updateDOM = (input) => {
 const mileage = (miles, gallons, price = 3.79) => {
   const MPG = Math.round(miles / gallons);
   const tripCost = gallons * price;
-  updateDOM("Miles per gallon is ${MPG} and trip cost is ${tripCost}");
-  myArr.push(MPG, tripCost);
 };
+const gastypes = ["Regular", "Preimum", "Disel"];
+const count = gastypes.push("eletric");
+console.log(count);
+console.log(gastypes);
 
 updateDOM(mileage(300, 10, 5.4));
 updateDOM(trackMPGCost(320, 12, 5));
 updateDOM(myArr);
+updateDOM(gastypes);
