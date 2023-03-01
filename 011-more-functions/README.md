8f2290ad1d4a5ce6f91c5e57d35a4114b5a9ba40
Hentsa999 <112977619+hentsa18@users.noreply.github.com>
2023-02-28 18:04:37 -0800

    learning about arrays

--- 011-more-functions/README.md
+++ 011-more-functions/README.md
@@ -2,3 +2,7 @@
 
 I am using devtools because I want to be able to fully learn javascript, altough It`s really tough, I want to keep trying and to slowly improve myself.
 Using devtools has showed me quite a lot I didnt know you can do with google chrome, it is confusing but I want to keep learning how to use devtools for coding.
+
+# My thoughts on Array`s!
+
+My understanding so far on Array`s are confusing but also very intresting on how they work and different types of methods.
--- 011-more-functions/main.js
+++ 011-more-functions/main.js
@@ -1,13 +1,21 @@
 //Update the DOM
+
+const myArr = ["Hentsa", 19];
+
 const updateDOM = (input) => {
   const divEl = document.querySelector("#output");
   const p = document.createElement("p");
   p.textContent = input;
-  divEl.appendChild("p");
+  divEl.appendChild(p);
 };
 
-const getDistance = (x1, y1, x2, y) => Math.sqrt(3 * 3 + 5 * 5);
-let y = 3 ^ (2 - 3) ^ 1;
-let x = 5 ^ (2 - 5) ^ 1;
+const mileage = (miles, gallons, price = 3.79) => {
+  const MPG = Math.round(miles / gallons);
+  const tripCost = gallons * price;
+  updateDOM("Miles per gallon is ${MPG} and trip cost is ${tripCost}");
+  myArr.push(MPG, tripCost);
+};
 
-updateDOM(getDistance(x1, y1, x2, y2));
+updateDOM(mileage(300, 10, 5.4));
+updateDOM(trackMPGCost(320, 12, 5));
+updateDOM(myArr);
