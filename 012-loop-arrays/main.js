6efb200f280ddc8ae69a1a39449d4819c036c05a
Hentsa999 <112977619+hentsa18@users.noreply.github.com>
2023-03-01 03:05:58 -0800

    my code for average trip cost

--- 012-loop-arrays/main.js
+++ 012-loop-arrays/main.js
@@ -7,16 +7,14 @@ const updateDOM = (input) => {
   divEl.appendChild(p);
 };
 
-const trackMPGCost = (miles, gallons, price = 3.79) => {
-  const MPG = Math.rounf(miles / gallons);
+const trackMPGandCost = (miles, gallons, price = 3.79) => {
+  const MPG = Math.round(miles / gallons);
   const tripCost = gallons * price;
   updateDOM("Miles per gallon is ${MPG} and trip cost is ${tripCost}");
   myArr.push(MPG, tripCost);
 };
 
 const calculateMPGAvg = () => {
-  //const avgMPG = (myArr[0] + myArr[2]) / 2;
-  //updateDOM("Average MPG is ${avgMPG}");
   let totalMPG = 0;
   for (let i = 0; i < myMPG.length; i++) {
     totalMPG = totalMPG + myMPG[i];
@@ -24,11 +22,13 @@ const calculateMPGAvg = () => {
   let avgMPG = totalMPG / myMPG.length;
   updateDOM("Average MPG is ${avgMPG}");
 };
-
-trackMPGCost(360, 15, 5.4);
-trackMPGCost(320, 12, 5);
-trackMPGCost(150, 10, 3);
-trackMPGCost(550, 20, 7.1);
-trackMPGCost(433, 14, 6);
-trackMPGCost(210, 11, 4.1);
+const totalCost = () => {
+  totalCost = totalMPG + myMPG;
+};
+trackMPGandCost(360, 15, 5.4);
+trackMPGandCost(320, 12, 5);
+trackMPGandCost(150, 10, 3);
+trackMPGandCost(550, 20, 7.1);
+trackMPGandCost(433, 14, 6);
+trackMPGandCost(210, 11, 4.1);
 calculateAvg();
