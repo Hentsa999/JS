76d0470a7dfa6c553aa2a29dbc2b60f5ca2fba00
Hentsa999 <112977619+hentsa18@users.noreply.github.com>
2023-02-28 16:34:32 -0800

    writing the mileage function

--- 011-more-functions/main.js
+++ 011-more-functions/main.js
@@ -1,9 +1,13 @@
-//Upddate the DOM
-const Hello = document.getElementbyId('Hello');
-const header = document.createElement('h1');
-const headerContent = document.createTextNode('JavaScript DOM Update');
-header.appendChild(headerContent);
-Hello.appendChild(header);
+//Update the DOM
+const updateDOM = (input) => {
+  const divEl = document.querySelector("#output");
+  const p = document.createElement("p");
+  p.textContent = input;
+  divEl.appendChild("p");
+};
 
+const getDistance = (x1, y1, x2, y) => Math.sqrt(3 * 3 + 5 * 5);
+let y = 3 ^ (2 - 3) ^ 1;
+let x = 5 ^ (2 - 5) ^ 1;
 
-
+updateDOM(getDistance(x1, y1, x2, y2));
