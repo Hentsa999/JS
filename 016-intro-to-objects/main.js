e9113c918ded39ba6518a4cf05ab416a272360c5
Hentsa999 <112977619+hentsa18@users.noreply.github.com>
2023-03-14 18:52:37 -0700

    this is mine

--- 016-intro-to-objects/main.js
+++ 016-intro-to-objects/main.js
@@ -4,18 +4,23 @@ const myArr = [];
 // console.log(typeof myArr)
 // console.log(myArr === myObj)
 const myNumber = 9;
-const myObj = {
-  name: "Hentsa",
-  age: "19",
-  hobbies: ["Gym", "Playing guitar", "Playing games"],
-  mobile: {
-    make: "pixel 7 pro",
-    Os: "android",
-  },
-  myGreeting: function (person) {
-    return "${this.name} who loves to ${this.hobbies[0]} says What is Up ${person}";
-  },
+const myWorkout = {
+  a: "Weightlifting",
 };
 
-const greeting = myObj.myGreeting("Chris");
-console.log(greeting);
+console.log(Object.values(myWorkout));
+//const myObj = {
+//name: "Hentsa",
+//age: "19",
+//hobbies: ["Gym", "Playing guitar", "Playing games"],
+//mobile: {
+// make: "pixel 7 pro",
+// Os: "android",
+// },
+// myGreeting: function (person) {
+//  return "${this.name} who loves to ${this.hobbies[0]} says What is Up ${person}";
+//},
+//};
+
+//const greeting = myObj.myGreeting("Chris");
+//console.log(greeting);
