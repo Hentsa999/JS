727698f54bd15f873e807f5ed043ca1837cdfb9c
Hentsa999 <112977619+hentsa18@users.noreply.github.com>
2023-03-16 03:40:47 -0700

    more refactoring

--- 019-refactor-arr-objs/main.js
+++ 019-refactor-arr-objs/main.js
@@ -13,8 +13,14 @@ const updateDOM = (input) => {
 
 const trackMPGandCost = (miles, gallons, price = 3.79) => {
   const MPG = Math.round(miles / gallons);
-  const tripCost = gallons * price;
+  const tripCost = Math.round(gallons * price);
   updateDOM("Miles per gallon is ${MPG} and trip cost is ${tripCost}");
+  const obj = {
+    MPG: MPG,
+    tripCost: tripCost,
+  };
+  return obj;
+
   //MY_MPG.push(MPG);
   //MY_TRIP_COST.push(tripCost);
 };
@@ -35,25 +41,38 @@ const calculateAvg = () => {
   updateDOM("Average Trip is ${avgTripCost}");
 };
 
+const isFormValid = (miles, gallons, price) => {};
+if (miles === 0 || gallons === 0 || price === 0) {
+  errMsg.push("Make sure your input value greater than 011, Try Again");
+}
+if (price > 1000) {
+  errMsg.push("really? I think this is in error... try again");
+}
+
+if (errMsg.length > 0) {
+  ERR.textContent = errMsg;
+  return false;
+} else {
+  return true;
+}
+
 FORM.addEventListener("submit", (e) => {
   e.preventDefault();
-  const errMsg = [];
   const miles = parseInt(e.target.miles.value);
   const gallons = parseInt(e.target.gallons.value);
   const price = parseInt(e.target.price.value);
-  if (miles === 0 || gallons === 0 || price === 0) {
-    errMsg.push("Make sure your input value greater than 011, Try Again");
-  }
-  if (price > 1000) {
-    errMsg.push("really? I think this is in error... try again");
-  }
 
-  if (errMsg.length > 0) {
-    ERR.textContent = errMsg;
-  } else {
+  const isValid = isFormValid(miles, gallons, price);
+  if (isValid) {
+    //const newDataObj = {
+    // miles: miles,
+    // gallons: gallons,
+    //  price: price,
+    // };
+
     ERR.textContent = "";
     AVG_OUTPUT.textContent = "";
-    trackMPGandCost(miles, gallons, price);
+    const updatedDataObj = trackMPGandCost(miles, gallons, price);
     calculateAvg();
   }
   FORM.reset();
